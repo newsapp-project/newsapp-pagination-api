e2f66c55ddf703925e03d23903723b8ce18d193e
philipszdavido <kurtwanger40@gmail.com>
2021-04-20 16:43:21 +0100

    Made updates

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fe974b2dc572ac72a1af4cde5ef94604'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'aa986b15-0e75-4587-8222-921d70f3ca95'
+};
